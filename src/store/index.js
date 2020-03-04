3de05d72c1c61cf3bd942efe3e8de8d94c18ba7e
coldboy002 <coldboy002@gmail.com>
2020-03-04 10:40:38 +0700

    chore(): add config redux-devtool

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,16 @@
-import { createStore,applyMiddleware } from 'redux';
-import rootReducer from '../reducers';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "../reducers";
+import thunk from "redux-thunk";
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer, {}, applyMiddleware(...middleware));
+const store = createStore(
+  rootReducer,
+  {},
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 export default store;
